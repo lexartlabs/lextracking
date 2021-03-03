e3e74f49259275b6fd5438845706b20b5c0e0be0
Juan_Rabazza <jrabazza@gmail.com>
2021-03-02 21:27:41 -0300

    upd

--- api/config/conn.php
+++ api/config/conn.php
@@ -18,7 +18,8 @@ Class Connection {
 	public function __construct(){
 		$servidor = "localhost";
 		$usuario  = "root";
-		$clave    = "";
+		$clave    = "root";
+		//$clave    = "";
 		$base	  = "lextracking_dev";
 
 		
--- app/src/env.js
+++ app/src/env.js
@@ -1,4 +1,5 @@
 //var BASE_URL = 'https://api.lexart.com.uy/lextracking-dev/';
-var BASE_URL = 'https://api.lexart.com.uy/lextracking/';
+//var BASE_URL = 'https://api.lexart.com.uy/lextracking/';
 var Trello_Key   = "2f132aeb2a02c90e0966cbcfd9f45329";
 var Trello_Token = "c9e22df4e936322b7949cc9b98c0e972f8991099f8bbb5733e70956abcd06ff4";
+var BASE_URL = 'http://localhost:82/lextracking/api/';
