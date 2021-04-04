32c15852d4e35ecdb26808138cbd1024b26cde0a
Nicolas <nicolas.machado@lexartlabs.com>
2021-04-04 19:35:43 -0300

    ignore env.js

--- api/README.md
+++
@@ -1,7 +0,0 @@
-# PHP Lite Framework - API Rest
-
-- Support: PHP 5.*
-- DB: MYSQL
-- Developed by [Lexart labs](http://lexartlabs.com) | Software Factory
-- Dev. lexcasa
-- © 2017
--- api/components/crypto/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	require("classes/Usuario.php");
-
-	$objUsr		= new Usuario();
-	$params 	= $match['params'];
-
-	echo json_encode( array("psw" => $objUsr->cryptoPsw($params["psw"])) );
-?>
