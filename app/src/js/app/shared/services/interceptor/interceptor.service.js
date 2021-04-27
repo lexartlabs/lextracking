4f6c3d8fb4f84bada05b1c47b0c5ccdb46fbbbe7
Oakland30 <federico.sanchez@lexartlabs.com>
2021-04-27 14:00:20 -0300

    fixed

--- app/src/js/app/shared/services/interceptor/interceptor.service.js
+++ app/src/js/app/shared/services/interceptor/interceptor.service.js
@@ -8,38 +8,41 @@
         return {
             response: function (response) {
                 // do something on success
+                var state = $injector.get('$state');
+                $rootScope.$watch('state', function(newState, oldState){
+                    if($window.localStorage["userId"] != $rootScope.userId ||
+                    $window.localStorage["userName"] != $rootScope.userName || 
+                    $window.localStorage["userEmail"] != $rootScope.userEmail || 
+                    $window.localStorage["userRole"] != $rootScope.userRole ||
+                    $window.localStorage["isAdmin"] != $rootScope.isAdmin ||
+                    $window.localStorage["isClient"] != $rootScope.isClient ||
+                    $window.localStorage["isDeveloper"] != $rootScope.isDeveloper){
+                        $window.localStorage.clear();
+                        state.go("login")
+                    }
+                }, true);
                 if(response.headers()['content-type'] == "application/json"){
                     if(response.status == 200){
                         if (typeof response.data !== 'undefined') {
                             if (response.data && response.data.response &&  !response.data.response.token && !response.data.response.email) {
-                                
-                                var state = $injector.get('$state');
-                                $rootScope.$watch('state', function(newState, oldState){
-                                    if($window.localStorage["userId"] != $rootScope.userId || $window.localStorage["userName"] != $rootScope.userName || $window.localStorage["userEmail"] != $rootScope.userEmail || $window.localStorage["userRole"] != $rootScope.userRole){
-                                        $window.localStorage.clear();
-                                        
-                                    }else{
-                                        var rest = $injector.get('UserServices');
-                                        rest.persistence( function(error, response){
-                                            var user = angular.copy(response);
-                                            console.log("SETEA LOCALSTORE::", user)
-                                            $window.localStorage[TOKEN_KEY]   = user.token;
-                                            $window.localStorage["userId"]    = user.id;
-                                            $window.localStorage["userName"]  = user.name;
-                                            $window.localStorage["userEmail"] = user.email;
-                                            $window.localStorage["userRole"]  = user.role;
-                                            $window.localStorage["isAdmin"]   = user.role == 'admin';
-                                            $window.localStorage["isClient"]  = user.role == 'client';
-                                            $window.localStorage["isDeveloper"]  = user.role == 'developer';
-                                            $window.localStorage["idUserClient"]  =user.idClient;
-                                            return;
-                                        });
-                                    }
-                                }, true);
-                                
+                                var rest = $injector.get('UserServices');
+                                rest.persistence( function(error, response){
+                                    var user = angular.copy(response);
+                                    console.log("SETEA LOCALSTORE::", user)
+                                    $window.localStorage[TOKEN_KEY]   = user.token;
+                                    $window.localStorage["userId"]    = user.id;
+                                    $window.localStorage["userName"]  = user.name;
+                                    $window.localStorage["userEmail"] = user.email;
+                                    $window.localStorage["userRole"]  = user.role;
+                                    $window.localStorage["isAdmin"]   = user.role == 'admin';
+                                    $window.localStorage["isClient"]  = user.role == 'client';
+                                    $window.localStorage["isDeveloper"]  = user.role == 'developer';
+                                    $window.localStorage["idUserClient"]  =user.idClient;
+                                    return;
+                                });
                             } else if(response.data.code === 401){
-                                console.log("LIMPIA LOCALSTORE")
                                 $window.localStorage.clear();
+                                state.go("login")                            
                             }
                         }
                     }                   
