fe93de11df4cf070d74cd6389468e2eb943406e2
Nicolas <nicolas.machado@lexartlabs.com>
2021-05-03 16:23:46 -0300

    [pl-62] add token and parameters

--- api/components/biller/post.php
+++ api/components/biller/post.php
@@ -7,29 +7,25 @@
 	
 	$params 	= json_decode(file_get_contents('php://input'), true);
 	$name 		= $match['name'];
+	
+	// GET USER BY ID
+	if($name == 'crear'){
+		$response = $objBiller->crear($conn,$params);
 
-	if($params){
-		// GET USER BY ID
-		if($name == 'crear'){
-			$response = $objBiller->crear($conn,$params);
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+	if($name == 'obtener'){
+		$response = $objBiller->obtener($conn,$params);
 
-			header('Content-Type: application/json');
-			echo json_encode($response);
-		}
-		if($name == 'obtener'){
-			$response = $objBiller->obtener($conn,$params);
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+	if($name == 'pdf'){
+		$response = $objBiller->pdf($conn,$params);
 
-			header('Content-Type: application/json');
-			echo json_encode($response);
-		}
-		if($name == 'pdf'){
-			$response = $objBiller->pdf($conn,$params);
-
-			header('Content-Type: application/json');
-			echo json_encode($response);
-		}
-	} else {
-		echo json_encode( array("response" => 'err') );
+		header('Content-Type: application/json');
+		echo json_encode($response);
 	}
 	
 ?>
