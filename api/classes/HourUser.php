91e2d769771bd6234bef2cbc7f8261cdcf20b247
Fede <federico.sanchez@lexartlabs.com>
2021-07-22 12:36:19 -0300

    [hour user] fix edit y delete

--- api/classes/HourUser.php
+++ api/classes/HourUser.php
@@ -40,38 +40,37 @@ class HourUser {
 	public function editUserFixedHours($conn, $params){
 
 		foreach ($params as $days){
-			foreach ($days as $day){
-				foreach ($day['horarios'] as $hour){
-					$sql = "UPDATE $this->model SET 'day' = ".$day['name'].", 'start' = ".$hour['start'].", 'end' = ".$hour['end']." WHERE user_id =".$params['user_id'];
+			if(count($days['horarios']) > 0 && $days['user_id']){
+				foreach ($days['horarios'] as $hour){
+					$sql = "UPDATE $this->model SET 'day' = '".$days['name']."', 'start' = '".$hour['start']."', 'end' = '".$hour['end']."' WHERE user_id =".$params['user_id'];
 					$d   = $conn->query($sql);
-				}
-			}
-		};
 
-		if (!empty($d)) {
-			return array("response" => $d);
-		} else {
-			return array("response" => 'Error al asignar proyecto');
-		}
-	}
-
-	public function deleteUserFixedHours($conn, $params){
-		foreach ($params as $days){
-			foreach ($days as $day){
-				foreach ($day['horarios'] as $hour){
-					if($hour['isDeleted'] == true){
-						$sql = "UPDATE $this->model SET 'updated_at' = now(), 'deleted_at' = now() WHERE user_hour_id =".$params['user_hour_id'];
-						$d   = $conn->query($sql);
+					if (!empty($d)) {
+						return array("response" => $d);
+					} else {
+						return array("response" => 'Error al asignar proyecto');
 					}
 				}
 			}
 		};
 
-		if (!empty($d)) {
-			return array("response" => $d);
-		} else {
-			return array("response" => 'Error al asignar proyecto');
-		}
+	}
+
+	public function deleteUserFixedHours($conn, $params){
+		foreach ($params as $days){
+				foreach ($days['horarios'] as $hour){
+					if($hour['isDeleted'] == true){
+						$sql = "UPDATE $this->model SET 'updated_at' = now(), 'deleted_at' = now() WHERE user_hour_id =".$hour['user_hour_id'];
+						$d   = $conn->query($sql);
+
+						if (!empty($d)) {
+							return array("response" => $d);
+						} else {
+							return array("response" => 'Error al asignar proyecto');
+						}
+					}
+				}
+		};
 	}
 
 }
