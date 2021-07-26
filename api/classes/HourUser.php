73861f5d65946d24b3fda6aa5d80b0130808ff02
Fede <federico.sanchez@lexartlabs.com>
2021-07-26 16:34:46 -0300

    [unnestable]

--- api/classes/HourUser.php
+++ api/classes/HourUser.php
@@ -24,12 +24,12 @@ class HourUser {
 		foreach ($params as $days){
 			if(count($days['horarios']) > 0){
 				var_dump($days);
-				foreach ($days['horarios'] as $hour){
-					var_dump($hour);
-					$sql = "INSERT INTO $this->model (user_id, day, start, end) VALUES (".$days['user_id'].",'".$days['name']."','".$hour['desde']."','".$hour['hasta']."')";
-					$d   = $conn->query($sql);
-					array_push($arrD, $d);
-				}
+				// foreach ($days['horarios'] as $hour){
+				// 	var_dump($hour);
+				// 	$sql = "INSERT INTO $this->model (user_id, day, start, end) VALUES (".$days['user_id'].",'".$days['name']."','".$hour['desde']."','".$hour['hasta']."')";
+				// 	$d   = $conn->query($sql);
+				// 	array_push($arrD, $d);
+				// }
 			}
 			if (!empty($arrD)) {
 				return array("response" => $arrD);
