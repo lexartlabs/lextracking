1b993dbeaae8019f5eb05fc2f315bd240ba4ce63
Developer <developer@lexartlabs.com>
2022-02-04 14:24:34 -0300

    feat: inner join clientName response [stable]

--- api-lumen/app/Http/Controllers/ProjectsController.php
+++ api-lumen/app/Http/Controllers/ProjectsController.php
@@ -13,11 +13,13 @@ class ProjectsController extends BaseController
 {
     public function all($id = null) 
     {
+        
         try{
             if(!empty($id)){
-               return Projects::where('id', $id)->first(); 
+               return Projects::join('clients', 'projects.idClient', '=', 'clients.id')->select('projects.*', 'clients.name as clientName')->where('projects.id', $id)->first(); 
             }
-            return Projects::all();
+
+            return Projects::join('clients', 'projects.idClient', '=', 'clients.id')->select('projects.*', 'clients.name as clientName')->get();
         }catch(Exception $e){
             return (new Response(array("Error" => BAD_REQUEST, "Operation" => "projects all"), 500));
         }
@@ -30,7 +32,7 @@ class ProjectsController extends BaseController
                 return (new Response(array("Error" => ID_INVALID, "Operation" => "projects client id"), 500));
             }
 
-            return Projects::where("idClient", $id)->get();
+            return Projects::join('clients', 'projects.idClient', '=', 'clients.id')->select('projects.*', 'clients.name as clientName')->where("idClient", $id)->get();
         }catch(Exception $e) {
             return (new Response(array("Error" => BAD_REQUEST, "Operation" => "projects client id"), 500));
         }
