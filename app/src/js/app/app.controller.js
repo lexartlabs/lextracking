ca29644488defeaca29dd9f32d02c32c41f98808
flavio <flasap@gmail.com>
2022-07-25 14:49:21 -0300

    update

--- app/src/js/app/app.controller.js
+++ app/src/js/app/app.controller.js
@@ -1,22 +1,22 @@
-(function(ng) {
+(function (ng) {
 
     'use strict';
-    
+
     var Module = ng.module('Imm');
 
-    Module.controller('AppCtrl', ['$scope', '$log', '$window', '$rootScope', '$state', '$translate', 'RestClient', 'ngDialog', function($scope, $log, $window, $rootScope, $state, $translate, RestClient, ngDialog) {
+    Module.controller('AppCtrl', ['$scope', '$log', '$window', '$rootScope', '$state', '$translate', 'RestClient', 'ngDialog', function ($scope, $log, $window, $rootScope, $state, $translate, RestClient, ngDialog) {
         // ---- Initialization
 
         $log.info('%cLEXTRACKING %cAPP STARTUP ', 'background: #203678; color: #fff; font-weight:bold; padding: 4px;', 'background: #666; color: #f9f9f9; padding: 4px;');
 
         // Put here stuff that can be deferred for the next digest
-        $scope.$evalAsync(function() {
+        $scope.$evalAsync(function () {
             $log.info("Async calls..");
         });
 
-        $rootScope.darkMode = 0;
+        $rootScope.darkMode = 1;
 
-        $rootScope.logout = function() {
+        $rootScope.logout = function () {
             $window.localStorage.clear();
             $rootScope.userName = '';
             $rootScope.token = '';
@@ -26,19 +26,19 @@
             $state.go('login');
         };
 
-        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
             //Close all dialogs
             ngDialog.closeAll();
         })
 
-        $scope.$on('$routeChangeSuccess', function(e, currentRoute) {
+        $scope.$on('$routeChangeSuccess', function (e, currentRoute) {
             //Change page title, based on Route information
             if (currentRoute && currentRoute.title) {
                 $window.title = currentRoute.title;
             }
         });
 
-        $scope.changeLanguage = function(langKey){
+        $scope.changeLanguage = function (langKey) {
             $translate.use(langKey);
         };
     }]);
