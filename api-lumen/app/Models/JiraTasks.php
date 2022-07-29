16b014aeac03960a997f364a128c686c4a576901
lucas-paz <lucas.hpaz@outlook.com>
2022-07-29 09:37:55 -0300

    PL-91 - fix - classes with wrong names

--- api-lumen/app/Models/JiraTasks.php
+++ api-lumen/app/Models/JiraTasks.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Banks extends Model
+class JiraTasks extends Model
 {
     use HasFactory;
 
