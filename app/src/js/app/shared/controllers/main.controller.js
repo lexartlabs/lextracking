9398c7cc1479606d3903a89b27f176149ca0c6bd
flavio <flasap@gmail.com>
2022-07-29 13:02:51 -0300

    update

--- app/src/js/app/shared/controllers/main.controller.js
+++ app/src/js/app/shared/controllers/main.controller.js
@@ -37,7 +37,7 @@
         });
     });
 
-    Module.controller('MainCtrl', ['$log', '$window', '$rootScope', '$scope', '$state', '$timeout', 'TracksServices', 'ProjectsServices', 'WeeklyHourServices', 'ngDialog', 'tasks_automaticServices', function ($log, $window, $rootScope, $scope, $state, $timeout, TracksServices, ProjectsServices, WeeklyHourServices, ngDialog, tasks_automaticServices) {
+    Module.controller('MainCtrl', ['$log', '$window', '$rootScope', '$scope', '$state', '$timeout', 'TracksServices', 'ProjectsServices', 'WeeklyHourServices', 'ngDialog', 'tasks_automaticServices', 'TasksServices', function ($log, $window, $rootScope, $scope, $state, $timeout, TracksServices, ProjectsServices, WeeklyHourServices, ngDialog, tasks_automaticServices, TasksServices) {
 
         $scope.thisHide = false;
         $scope.userToolsActive = false;
@@ -147,11 +147,14 @@
         };
 
         $scope.startTrack = function (task) {
+            if (!task) return
+            console.log("🚀  --> task", task)
+            // return false;
             WeeklyHourServices.find($scope.currentPage, $scope.query, function (err, weeklyHours, countItems) {
                 console.log(weeklyHours)
                 angular.forEach(weeklyHours, function (value, index) {
                     if (value.idUser == $rootScope.userId) {
-                        if(value.currency == null || value.currency == ''){
+                        if (value.currency == null || value.currency == '') {
                             value.currency = '$'
                         }
                         $scope.currency = value.currency
@@ -193,6 +196,14 @@
                             console.log('saved task', result);
                             $rootScope.currentTrack.id = result.id;
                             $scope.toggleTimer();
+                            if (!$rootScope.topBar.filterTask) {
+                                $rootScope.topBar.filterTask = $rootScope.currentTrack;
+                                $rootScope.topBar.filterTask.name = $rootScope.topBar.filterTask.taskName;
+                                $rootScope.topBar.filterTask.id = $rootScope.topBar.filterTask.idTask;
+                                if (!$rootScope.topBar.taskscondition || $rootScope.topBar.taskscondition.lenght == 0) {
+                                    $rootScope.topBar.taskscondition = [$rootScope.topBar.filterTask];
+                                }
+                            }
                         }
                     });
                 }
@@ -298,7 +309,7 @@
                     angular.forEach(weeklyHours, function (value, index) {
                         if (value.idUser == $rootScope.userId) {
                             console.log(value)
-                            if(value.currency == null || value.currency == ''){
+                            if (value.currency == null || value.currency == '') {
                                 value.currency = '$'
                             }
                             $scope.currency = value.currency
@@ -448,7 +459,7 @@
                                         console.log(value.costHour, 'costo hora idUser');
                                         var costo = parseInt(value.costHour);
                                         console.log('costo', costo);
-                                        if(value.currency == null || value.currency == ''){
+                                        if (value.currency == null || value.currency == '') {
                                             value.currency = '$'
                                         }
                                         $rootScope.currentTrack.currency = value.currency;
@@ -538,7 +549,7 @@
 
                             //Update current track
                             $rootScope.currentTrack = track;
-
+                            $rootScope.topBar.filterTask = $rootScope.currentTrack;
                             var now = new Date().getTime(); //Fecha actual millisegundos
                             var start = new Date(track.startTime).getTime(); //Fecha de track en millisegundos
                             var ms = now - start;
@@ -548,6 +559,34 @@
                 }
             });
         }
+
+
+        $rootScope.topBar = {};
+        $rootScope.topBar.filterTask = '';
+        $rootScope.topBar.filterTasks = [];
+        $rootScope.topBar.tasks = []
+
+        $rootScope.searchTasks = function (text) {
+
+            console.log("🚀  --> topBar.filterTask", $rootScope.topBar.filterTask)
+
+            if (text) {
+                $rootScope.topBar.filterTasks = { filter: [{ "name": text }], limit: 100, offset: 0 };
+                console.log("🚀  --> TEXT", $rootScope.topBar.filterTasks)
+                TasksServices.findByFilter($rootScope.topBar.filterTasks, function (err, tasks, countItems) {
+                    if (!err) {
+                        console.log('TEXT dsadsa tasks', tasks, countItems);
+                        $rootScope.topBar.tasks = tasks;
+                        console.log("🚀  --> TEXT $scope.topBar.tasks", $rootScope.topBar.tasks)
+                        // $scope.tasks = tasks.slice(0, PAGE_SIZE - 1);
+                        // $scope.total = countItems;
+                    }
+                });
+            }
+
+
+        };
+
 
     }]);
 
