d22399910e77efef05c429a797f514c6781a5761
lucasbrito3001 <lucas.brito3001@gmail.com>
2024-03-14 23:51:22 -0300

    style: Remove rename files and classes changes

--- application/api-lumen/app/app/Models/HourCost.php
+++ application/api-lumen/app/app/Models/HourCost.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HourCost extends Model
+class HoursCost extends Model
 {
     use HasFactory;
 
