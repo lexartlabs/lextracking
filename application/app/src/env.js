687c40ee40ae1f3fe7b69705e5697e4270b4bb66
root <root@lextracking.com>
2024-07-23 09:16:14 -0300

    fixes docker working on sv

--- application/app/src/env.js
+++ application/app/src/env.js
@@ -4,6 +4,6 @@ var FILES_BASE = ""
 //DEV
 // var BASE_URL = "https://dev.api.lextracking.com/api/"
 // var BASE_URL = "http://localhost:8081/api/"
-var BASE_URL = "http://localhost:82/api/"
+var BASE_URL = "http://165.227.176.9:82/api/"
 //PD
 // var BASE_URL = "https://api.lextracking.com/api/"
